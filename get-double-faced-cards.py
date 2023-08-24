6511fbec4adffc7dc6d85d0e0116bb8a9844a4cc
Stephan Kieburg <s.kieburg@lastrada.net>
2023-08-24 10:48:22 +0200

    Added small script to get up-to-date double-faced cards from scryfall, updated these in appsettings.json

---
+++ get-double-faced-cards.py
@@ -0,0 +1,16 @@
+# Small script to download scryfall data for names of double-faced cards
+
+import json
+import requests
+
+def filter_json(data, output_file):
+    filtered_data = [line for line in data['data'] if " // " in line]
+
+    with open(output_file, 'w') as f:
+        json.dump(filtered_data, f, indent=4)
+
+if __name__ == "__main__":
+    json_data = requests.get("https://api.scryfall.com/catalog/card-names").json()
+    output_file = "double-faced-cards.json"
+    filter_json(json_data, output_file)
+    print("Filtered data written to", output_file)
