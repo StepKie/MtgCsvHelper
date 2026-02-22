838a6af6f4df02afdccc0ec3e698621e8a0675fb
Stephan Kieburg <stephan.kieburg@gmail.com>
2026-02-22 17:21:17 +0100

    Apply bot review fixes: IDisposable, filename reset, no stack trace, IMtgApi in CardMapFactory ctor, round-trip test

--- MtgCsvHelper.Tests/DeckFormatTests.cs
+++ MtgCsvHelper.Tests/DeckFormatTests.cs
@@ -1,3 +1,5 @@
+using ScryfallApi.Client.Models;
+
 namespace MtgCsvHelper.Tests;
 
 [Collection(MtgApiCollection.Name)]
@@ -12,7 +14,32 @@ public class DeckFormatTests(MtgApiFixture fixture, ITestOutputHelper output) : 
 	//[InlineData("CARDKINGDOM")]
 	public void SupportedTest(string deckFormatName)
 	{
-		var classMap = new CardMapFactory(_config).GenerateClassMap(deckFormatName, _api);
+		var classMap = new CardMapFactory(_config, _api).GenerateClassMap(deckFormatName);
 		classMap.Should().NotBeNull();
+	}
+
+	[Theory]
+	[InlineData("DRAGONSHIELD")]
+	[InlineData("MOXFIELD")]
+	[InlineData("MANABOX")]
+	[InlineData("TOPDECKED")]
+	[InlineData("DECKBOX")]
+	public void StreamRoundTripTest(string deckFormatName)
+	{
+		var handler = new MtgCardCsvHandler(_api, _config, deckFormatName);
+		var original = new List<PhysicalMtgCard>
+		{
+			new() { Count = 2, Printing = new Card { Name = "Lightning Bolt", Set = "M11", SetName = "Magic 2011", CollectorNumber = "149" } }
+		};
+
+		var stream = new MemoryStream();
+		handler.WriteCollectionCsv(original, stream);
+		stream.Position = 0;
+
+		var parsed = handler.ParseCollectionCsv(stream).Cards;
+
+		parsed.Should().HaveCount(1);
+		parsed[0].Printing.Name.Should().Be("Lightning Bolt");
+		parsed[0].Count.Should().Be(2);
 	}
 }
